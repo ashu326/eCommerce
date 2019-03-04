a188c7adaefdd2ec902c9ba3444590a0f3d51aea
Ashutosh Chauhan <ashutoshchauhan326@gmail.com>
2019-03-04 13:41:46 +0530

    added sessions

--- app.js
+++ app.js
@@ -4,8 +4,13 @@ const http = require('http') ;
 const express = require('express') ;
 const bodyParser = require('body-parser');
 const session = require('express-session');
+const MongoDBStore = require('connect-mongodb-session')(session);
 
 const app = express() ;
+const store =new  MongoDBStore({
+    uri: 'mongodb://localhost:27017/shop',
+    collection: 'sessions' // where sessions will be stored
+});
 
 app.set('view engine', 'ejs') ;
 app.set('views', 'views') ;
@@ -16,7 +21,7 @@ const User = require('./models/user');
 
 app.use(bodyParser.urlencoded({extended: false})) ;
 app.use(express.static(path.join(__dirname, 'public'))) ;
-app.use(session({secret: 'my secret', resave: false, saveUninitialized: false}));
+app.use(session({secret: 'my secret', resave: false, saveUninitialized: false, store: store}));
 
 app.use((req, res, next) => {
     User.findById('5c744abb5dd33604e6e710b5')
--- controllers/auth.js
+++ controllers/auth.js
@@ -1,6 +1,6 @@
 exports.getLogin = (req, res) => {
   //const isLoggedIn =req.get('Cookie').split(';')[1].trim().split('=')[1];
-  console.log(req.session.isLoggedIn);
+    console.log(req.session.isLoggedIn);
     res.render('auth/login', {
       path: '/login',
       pageTitle: 'Login',
@@ -12,5 +12,12 @@ exports.postLogin = (req, res) => {
   //res.setHeader('Set-Cookie', 'loggedIn = true');
   req.session.isLoggedIn = true;
   res.redirect('/');
+};
+
+exports.postLogout = (req, res) => {
+  req.session.destroy(err => {
+    console.log(err);
+    res.redirect('/');
+  });
 };
  
