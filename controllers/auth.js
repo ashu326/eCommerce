5f1092507ebc13d030d332787d152b76d8d9069e
Ashutosh Chauhan <ashutoshchauhan326@gmail.com>
2019-06-14 14:02:06 +0530

    added sessions

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,11 +1,25 @@
+const User = require('../models/user');
 
 exports.getLogin = (req, res, next) => {
     res.render('auth/login', {
         path: '/login',
-        pageTitle: 'Login'
+        pageTitle: 'Login',
+        isAuthenticated: req.session.isLoggedIn
     });
 };
 
 exports.postLogin = (req, res, next) => {
-    res.redirect('/');
-}
+    User.findById('5c7ce58b4fe44c14d8526414')
+        .then(user => {
+            req.session.isLoggedIn = true;
+            req.session.user = user;
+            res.redirect('/');
+        });
+};
+
+exports.postLogout = (req, res, next) => {
+    req.session.destroy((err) => {
+        console.log(err);
+        res.redirect('/');
+    });
+};
