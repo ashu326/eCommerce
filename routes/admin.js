41aee1aa2b6579ef1fce04e3d06a948e2e9c7b00
Ashutosh Chauhan <ashutoshchauhan326@gmail.com>
2019-06-17 12:55:11 +0530

    added csrf token

--- routes/admin.js
+++ routes/admin.js
@@ -3,19 +3,20 @@ const path = require('path');
 const express = require('express');
 
 const adminController = require('../controllers/admin');
+const isAuth = require('../middleware/is-auth');
 
 const router = express.Router();
 
 // /admin/add-product => GET
-router.get('/add-product', adminController.getAddProduct);
+router.get('/add-product', isAuth, adminController.getAddProduct);
 
 // admin/products => GET
-router.get('/products', adminController.getProducts);
+router.get('/products', isAuth, adminController.getProducts);
 
 // admin/add-product => POST
 router.post('/add-product', adminController.postAddProduct);
 
-router.get('/edit-product/:productId', adminController.getEditProduct);
+router.get('/edit-product/:productId', isAuth, adminController.getEditProduct);
 
 router.post('/edit-product', adminController.postEditProduct);
 
